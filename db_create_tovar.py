1c724e5fa55d19d0066e6626001ca48cc730b646
pavelkoryagin <ankorkaluga@yandex.ru>
2025-03-08 19:41:44 +0300

    Вынесены файлы создания базы данных

--- db_create_tovar.py
+++ db_create_tovar.py
@@ -1,20 +1,32 @@
 from bot.config.config import load_config
 import psycopg
-
 config_crate_baza = load_config()
 baza = config_crate_baza.DBConfig.dbName
 user = config_crate_baza.DBConfig.dbUser
 password = config_crate_baza.DBConfig.dbPassword
 
 
+
 connection = psycopg.connect(dbname=baza,
                                  user=user,
                                  password=password)
-cursor = connection.cursor()
 connection.autocommit = True
-#sql = 'CREATE DATABASE MarketBot_ifw8e9238f0sd0j9eur029u3fj09we8jfsedf0s9ew0e9'
-sql = 'CREATE DATABASE marketbot'
+cursor = connection.cursor()
+sql = ("""
+    CREATE TABLE tovar (
+        id BIGSERIAL PRIMARY KEY,
+        fk_id_user BIGINT REFERENCES users(id) NULL,
+        fk_id_magazin INTEGER REFERENCES magazin(id) NULL,
+        url TEXT NULL,
+        url_parsing TEXT NULL,
+        artikul VARCHAR(50) NULL,
+        zena INTEGER NULL,
+        zena_new INTEGER NULL,
+        data TIMESTAMP NULL
+    )
+""")
 cursor.execute(sql)
 connection.commit()
 cursor.close()
 connection.close()
+#data TIMESTAMP WITH TIME ZONE NULL
--- db_create_user.py
+++ db_create_user.py
@@ -1,6 +1,5 @@
 from bot.config.config import load_config
 import psycopg
-
 config_crate_baza = load_config()
 baza = config_crate_baza.DBConfig.dbName
 user = config_crate_baza.DBConfig.dbUser
@@ -10,10 +9,16 @@ password = config_crate_baza.DBConfig.dbPassword
 connection = psycopg.connect(dbname=baza,
                                  user=user,
                                  password=password)
-cursor = connection.cursor()
 connection.autocommit = True
-#sql = 'CREATE DATABASE MarketBot_ifw8e9238f0sd0j9eur029u3fj09we8jfsedf0s9ew0e9'
-sql = 'CREATE DATABASE marketbot'
+cursor = connection.cursor()
+sql = ("""
+    CREATE TABLE users (
+        id BIGSERIAL PRIMARY KEY,
+        user_id BIGINT NULL,
+        data_registration DATE NULL,
+        blok_bot BOOLEAN NULL
+    ); 
+""")
 cursor.execute(sql)
 connection.commit()
 cursor.close()
